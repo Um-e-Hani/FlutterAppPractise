5e21a146eacb53d90e7758eafb5ed67b0774aa86
FlutLab.io <>
2022-01-23 13:25:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ), //AppBar
       body: Container(
+        padding: EdgeInsets.all(20.0),
         color: Colors.red[100],
         child: Text("Hello"), //Text
       ), //Container
