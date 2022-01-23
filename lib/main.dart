ab8ea0683e34778ed9033d41ea50c01e6cf07038
FlutLab.io <>
2022-01-23 11:54:55 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ void main() => runApp(MaterialApp(
               fontWeight: FontWeight.bold,
               letterSpacing: 2.0,
               color: Colors.grey[600],
+              fontFamily: 'IndieFamily',
             ), // TextStyle
           ), // Text
         ), //Center
