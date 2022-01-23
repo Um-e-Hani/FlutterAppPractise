0ca96b9fae44a45310cd73f1f86a1c26ea042304
FlutLab.io <>
2022-01-23 13:46:26 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ), //AppBar
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text("Hello World!"),
           FlatButton(
