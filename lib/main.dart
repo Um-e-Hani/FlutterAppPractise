7bd300ad1248383ef8697637fe411e5229bdff89
FlutLab.io <>
2022-01-23 12:04:29 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My first App"), //Text
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ), //AppBar
-        body: Center(
-          child: Text(
-            "Heyy",
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
-            ), // TextStyle
-          ), // Text
-        ), //Center
-        floatingActionButton: FloatingActionButton(
-          child: Text("Click!"),
-          onPressed: () {},
-          backgroundColor: Colors.red[600],
-        ), //FloatingActionButton
-      ), //home property
+      home: Home(),
     )); //material app
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My first App"), //Text
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ), //AppBar
+      body: Center(
+        child: Text(
+          "Heyy",
+          style: TextStyle(
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ), // TextStyle
+        ), // Text
+      ), //Center
+      floatingActionButton: FloatingActionButton(
+        child: Text("Click me!"),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ), //FloatingActionButton
+    ); //Scaffold
+  }
+}
