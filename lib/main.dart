4582932cd63d1347bd4fee659a95bb0ca9d88f1e
FlutLab.io <>
2022-01-23 14:12:24 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -74,26 +74,26 @@ class Home extends StatelessWidget {
                   color: Colors.red[600],
                   padding: EdgeInsets.all(50.0),
                 ), // Container
+              ],
+            ), //Row
+          ), //Container
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
                 Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        color: Colors.red[200],
-                        padding: EdgeInsets.all(50.0),
-                      ), // Container
-                      Container(
-                        color: Colors.red[600],
-                        padding: EdgeInsets.all(50.0),
-                      ), // Container
-                      Container(
-                        color: Colors.red[200],
-                        padding: EdgeInsets.all(50.0),
-                      ), // Container
-                    ],
-                  ), //Row
-                ), //Container
+                  color: Colors.red[200],
+                  padding: EdgeInsets.all(50.0),
+                ), // Container
+                Container(
+                  color: Colors.red[600],
+                  padding: EdgeInsets.all(50.0),
+                ), // Container
+                Container(
+                  color: Colors.red[200],
+                  padding: EdgeInsets.all(50.0),
+                ), // Container
               ],
             ), //Row
           ), //Container
