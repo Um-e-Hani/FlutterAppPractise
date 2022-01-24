61f1c959b5520508f6207e48caceeaf8bd6d2ca9
FlutLab.io <>
2022-01-24 08:39:10 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ), //AppBar
       body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
             flex: 7,
